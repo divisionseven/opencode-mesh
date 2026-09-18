0ea728e6e11e9e15b771e6c1814d8939375db37b
Division 7 <divisionseven@users.noreply.github.com>
2026-09-18 12:27:34 -0700

    fix(publish): bin path without ./ prefix
    Signed-off-by: Division 7 <divisionseven@users.noreply.github.com>

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 DIVISION 7 | MI-7 (@divisionseven)
 // SPDX-License-Identifier: MIT
 export const VERSION = "1.0.0";
-export const BUILD_ID = "dd83ad9-dirty";
-export const BUILD_TIME = "20260918T064335Z";
-export const BUILD_STAMP = "1.0.0+dd83ad9-dirty.20260918T064335Z";
+export const BUILD_ID = "d5aa013-dirty";
+export const BUILD_TIME = "20260918T192525Z";
+export const BUILD_STAMP = "1.0.0+d5aa013-dirty.20260918T192525Z";
